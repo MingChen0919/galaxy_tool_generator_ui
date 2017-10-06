232de420bdb4ad3a366da3546965a91268f8e01d
MChen <ming.chen0919@gmail.com>
2017-10-05 22:37:12 -0400

    adding galaxy toolshed api

--- GalaxyToolshed/GalaxyToolshed.php
+++ GalaxyToolshed/GalaxyToolshed.php
@@ -7,5 +7,16 @@
  * @file GalaxyToolshed/GalaxyToolshed.php
  */
 class GalaxyToolshed {
+  protected $key;
+
+  public function __construct() {
+    $this->key = '6650626edf6ef75b2ed1fbf21141a660';
+
+    dpm(GalaxyToolshedRequest::get('/api/repositories/get_repository_revision_install_info', [
+        'name' => 'test_galaxy_tool_generator',
+        'owner' => 'mingchen0919',
+        'changeset_revision' => '20bc2025c2ea',
+    ]));
+  }
 
 }
--- GalaxyToolshed/GalaxyToolshedRequest.php
+++ GalaxyToolshed/GalaxyToolshedRequest.php
@@ -7,4 +7,98 @@
  */
 class GalaxyToolshedRequest {
 
+  public static $toolshed_url = 'https://testtoolshed.g2.bx.psu.edu';
+
+  /**
+   * Create a GET request
+   *
+   * @param string $endpoint
+   * $param null $data
+   *
+   * @return mixed
+   */
+  public static function get($endpoint, $data = NULL) {
+    return static::send('GET', $endpoint, $data);
+  }
+
+  /**
+   * Create a POST request
+   *
+   * @param string $endpoint
+   * $param null $data
+   *
+   * @return mixed
+   */
+  public static function post($endpoint, $data = NULL) {
+    return static::send('GET', $endpoint, $data);
+  }
+
+  /**
+   * Create a DELETE request
+   *
+   * @param string $endpoint
+   * $param null $data
+   *
+   * @return mixed
+   */
+  public static function delete($endpoint, $data = NULL) {
+    return static::send('GET', $endpoint, $data);
+  }
+
+  /**
+   * Create a PUT request
+   *
+   * @param string $endpoint
+   * $param null $data
+   *
+   * @return mixed
+   */
+  public static function put($endpoint, $data = NULL) {
+    return static::send('GET', $endpoint, $data);
+  }
+
+  /**
+   * Create a curl HTTP request.
+   *
+   * @param string $method One of GET, PUT, POST or DELETE. Must be
+   *   capitalized.
+   * @param string $endpoint The url to call. If base_url is set, this value
+   *   will be appended to base_url.
+   * @param null|string|array $data
+   *
+   * @return mixed
+   */
+  public static function send($method, $endpoint, $data = NULL) {
+    if (!empty(static::$toolshed_url)) {
+      $url = static::$toolshed_url . $endpoint;
+    }
+    $curl = curl_init();
+    switch ($method) {
+      case "POST":
+        curl_setopt($curl, CURLOPT_POST, 1);
+        if ($data) {
+          curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        break;
+      case "DELETE":
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+        if ($data) {
+          curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        break;
+      case "PUT":
+        curl_setopt($curl, CURLOPT_PUT, 1);
+        break;
+      default:
+        if ($data) {
+          $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+        dpm(http_build_query($data));
+    }
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($result);
+  }
 }
