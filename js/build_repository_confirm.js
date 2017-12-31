641a92d8bd64f613e1f3b10f6365f569f277aade
Ming Chen <ming.chen0919@gmail.com>
2017-12-31 14:00:38 -0500

    add confirmation js

--- js/build_repository_confirm.js
+++ js/build_repository_confirm.js
@@ -4,7 +4,8 @@
     Drupal.behaviors.indices_delete_confirm = {
         attach: function (context, settings) {
             $(document).on('click', '#edit-build-repository', function (e) {
-                if (confirm("Warning: you can not undo this step!")) {
+                if (confirm("Warning: this will copy the registered XMLs to the Galaxy tool repository. " +
+                        "Existing XMLs will be overwritten with the updated XMLs. Are you sure you want to continue?")) {
                 }
                 else {
                     e.preventDefault();
